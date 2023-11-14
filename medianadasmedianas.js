251db6e169e21a72a12d0df6851606178f7d801b
DanRocha18 <daniel.rocha1207@gmail.com>
2023-11-14 20:22:09 -0300

    terminando o backend

--- medianadasmedianas.js
+++ medianadasmedianas.js
@@ -19,6 +19,24 @@ class MedianaDasMedianas {
       }
     }
   
+    calcularParaCasa(casa) {
+      const medianas = [];
+      this.fases.forEach((fase, index) => {
+        const pontuacoes = [];
+        for (const key in fase) {
+          if (key.includes(casa)) {
+            pontuacoes.push(fase[key][0], fase[key][1]);
+          }
+        }
+        const mediana = this.calcularMediana(pontuacoes);
+        medianas.push(mediana);
+        console.log(`Mediana das pontuações da casa ${casa} na Fase ${index + 1}: ${mediana}`);
+      });
+      const medianaDasMedianas = this.calcularMediana(medianas);
+      this.resultados[casa].push(medianaDasMedianas);
+      console.log(`Mediana das Medianas da casa ${casa} ao longo das fases: ${medianaDasMedianas}`);
+    }
+
     obterMedianaDasMedianasOrdenadas() {
         const medianaDasMedianas = [];
         for (const casa in this.resultados) {
