c2366bec10b787d9e2875a0300b5813df38e7a9a
DanRocha18 <daniel.rocha1207@gmail.com>
2023-11-13 21:49:41 -0300

    Finalizando Primeira tela

--- frontend/src/DisputaCasas.js
+++ frontend/src/DisputaCasas.js
@@ -6,36 +6,38 @@ const PlacaresCasas = () => {
     <div className="PlacaresCasas">
     <div className="body">
       <div className="Gryffindor">
-      <h2>Gryffindor</h2>
-      <h3>Vitórias: 4</h3>
-      <h3>Empates: 2</h3>
-      <h3>Derrotas: 0</h3>
-      <h3>Pontuação: 14</h3>
+      <h4>Gryffindor</h4>
+      <h5>Vitórias: 4</h5>
+      <h5>Empates: 2</h5>
+      <h5>Derrotas: 0</h5>
+      <h5>Pontuação: 14</h5>
       </div>
       <div className="Slytherin">
-        <h2>Slytherin</h2>
-        <h3>Vitórias: 4</h3>
-        <h3>Empates: 2</h3>
-        <h3>Derrotas: 0</h3>
-        <h3>Pontuação: 14</h3>
+        <h4>Slytherin</h4>
+        <h5>Vitórias: 4</h5>
+        <h5>Empates: 2</h5>
+        <h5>Derrotas: 0</h5>
+        <h5>Pontuação: 14</h5>
       </div>
       <div className="Ravenclaw">
-        <h2>Ravenclaw</h2>
-        <h3>Vitórias: 4</h3>
-        <h3>Empates: 2</h3>
-        <h3>Derrotas: 0</h3>
-        <h3>Pontuação: 14</h3>
+        <h4>Ravenclaw</h4>
+        <h5>Vitórias: 4</h5>
+        <h5>Empates: 2</h5>
+        <h5>Derrotas: 0</h5>
+        <h5>Pontuação: 14</h5>
       </div>
       <div className="Hufflepuff">
-        <h2>Hufflepuff</h2>
-        <h3>Vitórias: 4</h3>
-        <h3>Empates: 2</h3>
-        <h3>Derrotas: 0</h3>
-        <h3>Pontuação: 14</h3>
+        <h4>Hufflepuff</h4>
+        <h5>Vitórias: 4</h5>
+        <h5>Empates: 2</h5>
+        <h5>Derrotas: 0</h5>
+        <h5>Pontuação: 14</h5>
       </div>
     </div>
+    <div className="buttons">
     <button>Gerar Vencedor</button>
     <button>Começar Partidas</button>
+    </div>
     </div>
   );
 };
