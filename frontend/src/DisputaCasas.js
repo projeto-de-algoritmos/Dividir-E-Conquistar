2988050571ce0ea4dd9596d98e5d4514e28b4df6
Giulia Alcantara <alcantaragiubs@gmail.com>
2023-11-20 14:39:11 -0300

    arrumando o projeto

--- frontend/src/DisputaCasas.js
+++ frontend/src/DisputaCasas.js
@@ -3,16 +3,18 @@ import { Link } from 'react-router-dom';
 
 const PlacaresCasas = () => {
   return (
-    <div className="PlacaresCasas">
+    <div className="placares">
       <div className="dumb"/>
-      <div className='text-db'>
-        <h3>"A Glória eterna! Isto é o que aguarda o estudante que vencer o torneio tribruxo, mas para isso, ele precisará sobreviver em três tarefas. Três tarefas extremamente perigosas."</h3>
-      </div>
-      <div className="buttons">
-        <Link to="/gere-partidas">
-          <button className='partidas'>Começar Partidas</button>
-        </Link>
-      </div>
+      <div>
+        <div className='text-db'>
+          <h3>"A Glória eterna! Isto é o que aguarda o estudante que vencer o torneio tribruxo, mas para isso, ele precisará sobreviver em três tarefas. Três tarefas extremamente perigosas."</h3>
+        </div>
+        <div className="buttons">
+          <Link to="/gere-partidas">
+            <button className='partidas'>Iniciar Torneio</button>
+          </Link>
+        </div>
+      </div>  
     </div>
   );
 };
--- frontend/src/HeaderComponente.js
+++ frontend/src/HeaderComponente.js
@@ -3,7 +3,10 @@ import React from 'react';
 const HeaderComponent = () => {
   // Estilos para o cabeçalho
   return (
-    <div className  ='App-header'></div>
+    <div className  ='App-header'>
+      <h1>Harry Potter e O Cálice de Fogo</h1>  
+      <h2>O Torneio Tribruxo</h2>
+    </div>
   );
 };
 
