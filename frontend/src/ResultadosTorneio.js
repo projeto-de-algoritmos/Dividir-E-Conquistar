1833d6f2a5c4dbc4d80184319baa4490e5b02ae6
Giulia Alcantara <alcantaragiubs@gmail.com>
2023-11-19 21:11:15 -0300

    arrumando o back quase todo

--- frontend/src/ResultadosTorneio.js
+++ frontend/src/ResultadosTorneio.js
@@ -3,14 +3,24 @@ import './App.css';
 import './GerePartidas.css';
 import { Link } from 'react-router-dom';
 import HeaderComponent from './HeaderComponente';
+import { medianaDasMedianasOrdenadas } from './medianadasmedianas';
 
 const ResultadosTorneio = () => {
+
+  const [resultado, setResultado] = useState([]);
+
+  useEffect(() => {
+    setResultado(medianaDasMedianasOrdenadas);
+    console.log("RESULTADO: ", medianaDasMedianasOrdenadas);
+  }, []);
+
   return (
     <div className="ResultadosTorneio">
       <HeaderComponent />
       <div className="body">
         <div className="Podium">
             <h2 className="primeiro">Primeiro</h2>
+
             <h2 className="segundo">Segundo</h2>
             <h2 className="terceiro">Terceiro</h2>
             <h2 className="quarto">Quarto</h2>
--- frontend/src/medianadasmedianas.js
+++ frontend/src/medianadasmedianas.js
@@ -1,63 +1,95 @@
-const { fasesDePontuacoes } = require('./pontuacoes.dto');
-
-
-class MedianaDasMedianas {
-    constructor(fases) {
-      this.fases = fases;
-      this.resultados = {
-        Grifinória: [],
-        Sonserina: [],
-        LufaLufa: [],
-        Corvinal: []
-      };
-    }
-  
-    calcularMediana(arr) {
-      const sorted = arr.sort((a, b) => a - b);
-      const middle = Math.floor(sorted.length / 2);
-      if (sorted.length % 2 === 0) {
-        return (sorted[middle - 1] + sorted[middle]) / 2;
-      } else {
-        return sorted[middle];
-      }
-    }
-  
-    calcularParaCasa(casa) {
-      const medianas = [];
-      this.fases.forEach((fase, index) => {
-        const pontuacoes = [];
-        for (const key in fase) {
-          if (key.includes(casa)) {
-            pontuacoes.push(fase[key][0], fase[key][1]);
-          }
-        }
-        const mediana = this.calcularMediana(pontuacoes);
-        medianas.push(mediana);
-        console.log(`Mediana das pontuações da casa ${casa} na Fase ${index + 1}: ${mediana}`);
-      });
-      const medianaDasMedianas = this.calcularMediana(medianas);
-      this.resultados[casa].push(medianaDasMedianas);
-      console.log(`Mediana das Medianas da casa ${casa} ao longo das fases: ${medianaDasMedianas}`);
-    }
-
-    obterMedianaDasMedianasOrdenadas() {
-        const medianaDasMedianas = [];
-        for (const casa in this.resultados) {
-          const mediana = this.calcularMediana(this.resultados[casa]);
-          medianaDasMedianas.push(mediana);
-        }
-        medianaDasMedianas.sort((a, b) => b - a); // Ordena de forma decrescente
-        return medianaDasMedianas;
-      }
+// Função para calcular a mediana das medianas
+function medianaDasMedianas(arr) {
+  const grupos = [];
+  for (let i = 0; i < arr.length; i += 5) {
+      grupos.push(arr.slice(i, i + 5));
   }
-  
-const torneio = new MedianaDasMedianas(fasesDePontuacoes);
-torneio.calcularParaCasa('Grifinória');
-torneio.calcularParaCasa('Sonserina');
-torneio.calcularParaCasa('LufaLufa');
-torneio.calcularParaCasa('Corvinal');
 
-const medianaDasMedianasOrdenadas = torneio.obterMedianaDasMedianasOrdenadas();
-console.log(medianaDasMedianasOrdenadas);
+  const medianas = grupos.map(grupo => {
+      return grupo.sort((a, b) => a - b)[Math.floor(grupo.length / 2)];
+  });
+
+  const medianaDasMedianas = selecionar(medianas, Math.floor(medianas.length / 2));
+
+  return medianaDasMedianas;
+}
+
+// Função para selecionar o k-ésimo menor elemento em um array
+function selecionar(arr, k) {
+  if (arr.length === 1) {
+      return arr[0];
+  }
+
+  const grupos = [];
+  for (let i = 0; i < arr.length; i += 5) {
+      grupos.push(arr.slice(i, i + 5));
+  }
+
+  const medianas = grupos.map(grupo => {
+      return grupo.sort((a, b) => a - b)[Math.floor(grupo.length / 2)];
+  });
+
+  const pivo = selecionar(medianas, Math.floor(medianas.length / 2));
+
+  const menores = [];
+  const maiores = [];
+  const iguais = [];
+
+  arr.forEach(num => {
+      if (num < pivo) {
+          menores.push(num);
+      } else if (num > pivo) {
+          maiores.push(num);
+      } else {
+          iguais.push(num);
+      }
+  });
+
+  if (k < menores.length) {
+      return selecionar(menores, k);
+  } else if (k < menores.length + iguais.length) {
+      return iguais[0];
+  } else {
+      return selecionar(maiores, k - menores.length - iguais.length);
+  }
+}
+
+function gerarValoresAleatorios(numRodadas) {
+  var valoresAleatorios = [];
+
+  for (var i = 0; i < numRodadas; i++) {
+      // Gera um valor aleatório entre 0 e 100
+      var valorAleatorio = Math.floor(Math.random() * 101);
+      valoresAleatorios.push(valorAleatorio);
+  }
+
+  return valoresAleatorios;
+}
+
+function ordenarValores(a, b, c, d) {
+  // Cria um array com os quatro valores
+  let valores = [a, b, c, d];
+
+  // Usa o método sort para ordenar os valores
+  valores.sort(function (x, y) {
+    return x - y;
+  });
+
+  // Retorna o array ordenado
+  return valores;
+}
+
+// Exemplo de uso
+let valoresOrdenados = ordenarValores(4, 2, 7, 1);
+console.log(valoresOrdenados);
 
 
+// Exemplo de uso da função
+var numRodadas = 5
+var resultados = gerarValoresAleatorios(numRodadas);
+console.log("ARRAY: ", resultados)
+
+
+torneio = medianaDasMedianas(resultados)
+
+console.log("A mediana das medianas desse array é: ", torneio)
